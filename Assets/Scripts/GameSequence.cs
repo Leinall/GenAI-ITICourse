5dc917c625f5700c726ac2b9abd027fc990164d7
Leinall <leina.elansary@gmail.com>
2025-10-26 00:45:48 +0300

    updating the game sequence ans the scene

--- Assets/Scripts/GameSequence.cs
+++ Assets/Scripts/GameSequence.cs
@@ -1,4 +1,13 @@
+using UnityEngine;
+
 public class GameSequence : StateManager
 {
 
+    public Transform Camera;
+
+    public void GoToWayPoint(Transform transform)
+    {
+        Camera.transform.parent = transform;
+        Camera.transform.localPosition = Vector3.zero;
+    }
 }
